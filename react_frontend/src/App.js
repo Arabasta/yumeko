7b4d15dedd8a9dc41390d0409437e01b96fc9288
Arabasta <keiyamx@gmail.com>
2024-07-14 01:51:15 +0800

    frontend feat: complete react frontend

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Form from './components/Form';
+import Result from './components/Result';
+import ErrorMessage from './components/ErrorMessage';
+import Footer from "./components/footer";
+import handleError from './utils/handleHttpError';
 
 function App() {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (data) => {
+    setError(null);
+    setResult(null);
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/v1/coin-change`, data);
+      setResult(response.data.coins);
+    } catch (err) {
+        setError(handleError(err));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App container">
+        <h1 className="mt-5">Coin Change Kei Yam</h1>
+        <Form onSubmit={handleSubmit} />
+        {result && <Result coins={result} />}
+        {error && <ErrorMessage message={error} />}
+        <Footer />
+      </div>
   );
 }
 
