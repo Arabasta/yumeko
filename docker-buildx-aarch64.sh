f08d97c050e95cc9156122b6d4561c6136974b47
Arabasta <keiyamx@gmail.com>
2025-04-01 21:26:43 +0800

    update script

--- docker-buildx-aarch64.sh
+++ docker-buildx-aarch64.sh
@@ -8,3 +8,5 @@ docker buildx create --use
 
 # build spring backend for linux
 docker buildx build --platform linux/arm64 -t spring-backend:latest -f ./spring_backend/Dockerfile ./spring_backend --load
+
+docker compose up
